94f8f5b951c8d9c44e481a8b2e0634275c43b730
kenzieadmin <kenzieadmin@users.noreply.github.com>
2023-07-03 03:58:51 +0000

    deploy: 2d85f8982a77384e6a5b6c73a0c0ea0a9ff42eda

--- code.js
+++ code.js
@@ -2,3 +2,16 @@
 // and select "Open with Live Server"
 
 // Your Code Here.
+function userLocation(location) {
+    let latitude = location.coords.latitude;
+    let longitude = location.coords.longitude;
+
+    document.write(`Latitude: ${latitude} Longitude: ${longitude}`)
+}
+
+if (navigator.geolocation) {
+    navigator.geolocation.watchPosition(userLocation);
+} else {
+    document.write(`Sorry we are unable to detect your location... Did you know the Latitude and Longitude of Tokyo, Japan is 35.6762° N, 139.6503° E?`)
+}
+
