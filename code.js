6cd36d93ed6727f2958bb395a1ba06ff3cc79638
kenzieadmin <kenzieadmin@users.noreply.github.com>
2023-07-06 03:56:58 +0000

    deploy: 21704516012cea871bc2804a4a01ecb5255e8191

--- code.js
+++ code.js
@@ -15,3 +15,12 @@ if (navigator.geolocation) {
     document.write(`Sorry we are unable to detect your location... Did you know the Latitude and Longitude of Tokyo, Japan is 35.6762° N, 139.6503° E?`)
 }
 
+// Constructing the query URL: https://flickr.com/services/rest/?api_key=437662e6f4e7a651726c9ca1aa27b022&format=json&nojsoncallback=1&method=flickr.photos.search&safe_search=1&per_page=5&lat=39.76574&lon=-86.1579024&text=dog
+
+
+let imageSearchBar = document.getElementById("imageSearchBar");
+let searchButton = document.getElementById("searchButton");
+
+searchButton.addEventListener("click", () => {
+    // Fetch the Flickr API and return results
+})
